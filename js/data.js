38741eabc9bfb10233f266e27db5f6b3fdac6e9e
jaikathuria <jaikathuria@live.com>
2017-03-16 15:12:07 +0530

    Add: Data

---
+++ js/data.js
@@ -0,0 +1,82 @@
+data = [
+  {
+    id: "square",
+    color: "red",
+    icon: "fa-square-o"
+  },
+  {
+    id: "square",
+    color: "red",
+    icon: "fa-square-o"
+  },
+  {
+    id: "circle",
+    color: "green",
+    icon: "fa-circle-o"
+  },
+  {
+    id: "circle",
+    color: "green",
+    icon: "fa-circle-o"
+  },
+  {
+    id: "dimond",
+    color: "purple",
+    icon: "fa-diamond"
+  },
+  {
+    id: "dimond",
+    color: "purple",
+    icon: "fa-diamond"
+  },
+  {
+    id: "superpower",
+    color: "orange",
+    icon: "fa-superpowers"
+  },
+  {
+    id: "superpower",
+    color: "orange",
+    icon: "fa-superpowers"
+  },
+  {
+    id: "magic",
+    color: "cyan",
+    icon: "fa-magic"
+  },
+  {
+    id: "magic",
+    color: "cyan",
+    icon: "fa-magic"
+  },
+  {
+    id: "heart",
+    color: "pink",
+    icon: "fa-heart-0"
+  },
+  {
+    id: "heart",
+    color: "pink",
+    icon: "fa-heart-0"
+  },
+  {
+    id: "star",
+    color: "yellow",
+    icon: "fa-star-0"
+  },
+  {
+    id: "star",
+    color: "yellow",
+    icon: "fa-star-0"
+  },
+  {
+    id: "minus",
+    color: "grey",
+    icon: "fa-minus"
+  },
+  {
+    id: "minus",
+    color: "grey",
+    icon: "fa-minus"
+  }
+];
