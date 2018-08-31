048b986b2a131ed07de06082778cd562bbe8ebd6
jaikathuria <jaikathuria@live.com>
2018-08-31 06:42:37 +0530

    Remove unneccessary files, add css libraries

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+
+/* Import CSS Librarie */
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'animate.css/animate.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+
+/* Import Styles */ 
+import './styles/App.css'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
