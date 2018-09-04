5dba4f9db60e66f70c29b3ef5ddeefe1f8efc75f
jaikathuria <jaikathuria@live.com>
2018-09-04 23:49:26 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,13 @@
+importScripts("/matchit/precache-manifest.9552aeffb1c00e7b924caf335be7ca7a.js", "/matchit/workbox-v3.4.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/matchit/workbox-v3.4.1"});
+// See https://developers.google.com/web/tools/workbox/guides/configure-workbox
+workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+
+// We need this in Webpack plugin (refer to swSrc option): https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin#full_injectmanifest_config
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+// app-shell
+workbox.routing.registerRoute("/", workbox.strategies.cacheFirst());
