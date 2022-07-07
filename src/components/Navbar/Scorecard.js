ec469c1a4730c2fbc37f4bcb4b2bd73913789179
Jai Kathuria <jai.kathuria@smallcase.com>
2022-07-07 13:49:05 +0530

    feat(navbar): star rating added to navbar during the gameplay

--- src/components/Navbar/Scorecard.js
+++ src/components/Navbar/Scorecard.js
@@ -8,6 +8,13 @@ export default class Scorecard extends Component {
 			<div className="col-xs-8">
 				<div className="navbar-header pull-right">
 					<div className="row">
+						<p class="navbar-brand brand small">
+							<span id="stars">
+								<i className="fa fa-star"></i>
+								<i className={`fa ${moves > 14 ? 'fa-star-o' : 'fa-star' }`}></i>
+								<i className={`fa ${moves > 10 ? 'fa-star-o' : 'fa-star' }`}></i>
+							</span>
+						</p>
 						<p className="navbar-brand brand small"> MOVES:<span className="head moves"> {moves}</span></p>
 						<p className="navbar-brand brand small"> TIME:<span className="head time"> {time}</span></p>
 					</div>
